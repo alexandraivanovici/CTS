4c1881d55392f6a2b6c031c04669add1f5dd34cc
alexandraivanovici <ivanoviciioana18@stud.ase.ro>
2021-04-17 10:54:21 +0300

    Facade

---
+++ seminar8/src/ro/ase/cts/facade/clase/Facade.java
@@ -0,0 +1,20 @@
+package ro.ase.cts.facade.clase;
+
+public class Facade {
+    public static boolean potInvitaOameniiLasMasa(Masa masa){
+        if(OperatorMese.esteLibera(masa)) {
+            Picolo jimmy= new Picolo("Jimmy");
+            if(jimmy.esteDebarasata(masa) && jimmy.esteAranjata(masa)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
+
+
+    }
+}
---
+++ seminar8/src/ro/ase/cts/facade/clase/OperatorMese.java
@@ -0,0 +1,10 @@
+package ro.ase.cts.facade.clase;
+
+public class OperatorMese {
+
+    public static boolean esteLibera(Masa masa) {
+        return masa.getNrPersoane() == 0;
+    }
+
+
+}
